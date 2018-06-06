cee0eb61a7b03b3141631b1b36d720f51226f763
Tanner Davis <tannerdavisr@gmail.com>
2018-06-06 17:01:42 -0600

    IO Helper Classes and other class changes
    Created the FileWriter and FileReader class to help with file IO and creation/interpretation of Images.  
     Added a couple more skeleton functions that I need to implement/learn more about. Also changed how the braces are done.

--- lab1/src/com/rbdavis/java/Pixel.java
+++ lab1/src/com/rbdavis/java/Pixel.java
@@ -9,52 +9,62 @@ public class Pixel {
 
     final int NUM_COLORS = 3;
 
-    public Pixel(int r, int g, int b) {
+    public Pixel(int r, int g, int b)
+    {
         this.r = new Color(r);
         this.g = new Color(g);
         this.b = new Color(b);
     }
 
-    public Color getR() {
+    public Color getR()
+    {
         return r;
     }
 
-    public Color getG() {
+    public Color getG()
+    {
         return g;
     }
 
-    public Color getB() {
+    public Color getB()
+    {
         return b;
     }
 
-    public void setRGB(int red, int green, int blue) {
+    public void setRGB(int red, int green, int blue)
+    {
         this.r.setVal(red);
         this.g.setVal(green);
         this.b.setVal(blue);
     }
 
-    public void invert() {
+    public void invert()
+    {
         this.r.invert();
         this.g.invert();
         this.b.invert();
     }
 
-    public void grayscale() {
-        int avg = (this.r.getVal() + this.g.getVal() + this.b.getVal()) / NUM_COLORS;
+    public void grayscale()
+    {
+        int avg = ( this.r.getVal() + this.g.getVal() + this.b.getVal() ) / NUM_COLORS;
         this.r.setVal(avg);
         this.g.setVal(avg);
         this.b.setVal(avg);
     }
 
-    public void reset(int newR, int newG, int newB) {
+    public void reset(int newR, int newG, int newB)
+    {
 
     }
 
-    public boolean equals(Pixel other) {
+    public boolean equals(Pixel other)
+    {
         return (this.r == other.r) && (this.g == other.g) && (this.b == other.b);
     }
 
-    public String toString() {
+    public String toString()
+    {
         StringBuilder sb = new StringBuilder();
         sb.append(this.r.getVal());
         sb.append(" ");
