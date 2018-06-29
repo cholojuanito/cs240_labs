5c8faf8814b30c2a06e1d873069bbb1b4b35363b
Tanner Davis <tannerdavisr@gmail.com>
2018-06-29 14:15:17 -0600

    Error handling in Main
    Added a try-catch-finally block to handle anything that might make its way back to Main.

--- lab2/src/com/rbdavis/java/spell/Main.java
+++ lab2/src/com/rbdavis/java/spell/Main.java
@@ -3,32 +3,42 @@ package com.rbdavis.java.spell;
 import java.io.IOException;
 
 class Main
-
-    // ADD src\com\rbdavis\java\spell\ before the text file
 {
     public static void main(String[] args)
     {
+        if(args.length < 2)
+        {
+            System.out.println("Insufficient amount of arguments. Please check your arguments.");
+            return;
+        }
         String dictionaryFileName = args[0];
         String inputWord = args[1];
-
+        String suggestion = null;
         ISpellCorrector corrector = new SpellCorrector();
 
         try
         {
             corrector.useDictionary(dictionaryFileName);
-            String suggestion = corrector.suggestSimilarWord(inputWord);
+            suggestion = corrector.suggestSimilarWord(inputWord);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Was not able to read " + dictionaryFileName + ".\n"
+                    + "IOException: " + e.getMessage()
+            );
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something caused the program to end early:\n Exception:");
+            System.out.println(e.getMessage());
+        }
+        finally {
             if (suggestion == null)
             {
                 suggestion = "No similar word found";
             }
 
             System.out.println("Suggestion is: " + suggestion);
-        }
-        catch(IOException e)
-        {
-            System.out.println("Was not able to read " + dictionaryFileName + ".\n"
-                    + "IOException: " + e.toString()
-            );
         }
     }
 }
